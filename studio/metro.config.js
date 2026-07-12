dbb17b07d794084849d582628cd3d06acf167073
tstyas56-art <tstyas56@gmail.com>
2026-07-12 04:06:37 +0300

    Delete studio/metro.config.js

--- studio/metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver.assetExts.push('db', 'sqlite');
-
-module.exports = config;
